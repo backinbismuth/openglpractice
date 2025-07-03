ddc4068f5c0c84a569a5372ff44067d8c521a766
Bismuth <132019958+backinbismuth@users.noreply.github.com>
2025-07-03 16:00:17 +0100

    Update main.cpp
    Now displays a colour triangle. Also frees memory more.

--- main.cpp
+++ main.cpp
@@ -5,13 +5,17 @@
 // Shaders below
 const char* vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
+"layout (location = 1) in vec3 aColor;\n"
+"out vec3 ourColor	;\n"
 "void main() {\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);}\0";
+"   gl_Position = vec4(aPos, 1.0);\n"
+"	ourColor = aColor;}\0";
 
 const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
+"in vec3 ourColor;\n"
 "void main() {\n"
-"   FragColor = vec4(1.0f,0.5f,0.7f,1.0f);}\0";
+"   FragColor = vec4(ourColor, 1.0);}\0";
 
 
 void processInput(GLFWwindow* window) { // Processes input
@@ -37,7 +41,7 @@ int main() {
 		return -1;
 	}
 	glfwMakeContextCurrent(window); // Makes window current context (Forefront at screen)
-	
+
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) { // Fancy way to say "if process for glad doesnt exist"
 		std::cout << "Failed to initialise GLAD :[" << std::endl;
 		return -1;
@@ -46,10 +50,16 @@ int main() {
 	glViewport(0, 0, 800, 600);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-	float vertices[] = { // Triangle vertices code
-	-0.5f, -0.5f, 0.0f,
-	0.5f, -0.5f, 0.0f,
-	0.0f, 0.5f, 0.0f
+	float vertices[] = {
+		// positions         // colors
+		 0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
+		-0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
+		 0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
+	};
+
+	unsigned int indices[] = {
+		0,1,3, // first tri
+		1,2,3 // second tri (only applicable with rectangles! We're not using our EBO rn so it doesnt matter)
 	};
 
 	unsigned int VBO; // VBO = Vertex Buffer Object
@@ -78,8 +88,20 @@ int main() {
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+
+	unsigned int EBO; // Element Buffer Object (Currently unused, since we're not rendering a bunch of verts
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0); // For Position
 	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float))); // For Color
+	glEnableVertexAttribArray(1);
+
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); // Uncomment to make wireframe
 
 	while (!glfwWindowShouldClose(window)) { // Render loop
 		// Input
@@ -88,16 +110,28 @@ int main() {
 		// Rendering Commands
 		glClearColor(0.5f, 0.2f, 0.7f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
-
 		glUseProgram(shaderProgram);
+
+		float timeValue = glfwGetTime();
+		float colorValue = sin(timeValue) / 2.0f + 0.5f;
+		int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+		glUniform4f(vertexColorLocation, colorValue, 0.0f, colorValue - 0.1f, 1.0f);
+
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(0);
 
 		// Check events, swap buffers
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
+	glDeleteVertexArrays(1, &VAO); // Free up memory
+	glDeleteBuffers(1, &VBO);
+	glDeleteProgram(shaderProgram);
+
 	glfwTerminate();
+
+	std::cout << "Everything worked! :]\n";
 	return 0;
 }
